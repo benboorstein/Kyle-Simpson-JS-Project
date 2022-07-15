1956a06782fb71d777f5c6b41c27126108b95742
Ben <benboorstein@gmail.com>
2022-07-15 15:17:42 -0400

    Minor changes. Exercise finished.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,41 @@
+// maybe a future exercise: redo just with a real API, like from https://icanhazdadjoke.com/
+
+
 class Bookshelf {
   constructor() {
     this.favoriteBooks = []
-  }
-  addFavoriteBook(bookName) {
-    if (!bookName.includes("Great")) {
+  } // no commas just because of how the 'class' syntax is
+  addFavoriteBook(bookName) { // in the format more familiar to me: 'addFavoriteBook: function(bookName)...'
+    if (!bookName.includes('Great')) {
         this.favoriteBooks.push(bookName)
     }
   }
   printFavoriteBooks() {
-    console.log(`Favorite Books: ${String(this.favoriteBooks.length)}`) // Note that without "String()", 'favoriteBooks.length' is still getting coerced from a number to a string (because it's being put into a string), but passing it into 'String()' makes that number-to-string coercion explicit, just so it's clear what's happening when you look at it later. Simpson advises this sort of thing to increase clarity.
+    console.log(`Favorite Books: ${String(this.favoriteBooks.length)}`) // Note that without "String()", 'favoriteBooks.length' is still getting coerced from a number to a string (because it's being put into a string), but passing it into 'String()' makes the number-to-string coercion *explicit*, just so it's clear what's happening when you look at it later. Simpson advises this sort of thing to increase clarity. Robert, too.
     for (let bookName of this.favoriteBooks) {
         console.log(bookName)
     }
   }
 }
 
-let BOOK_API = "https://some.url/api"
+let BOOK_API = 'https://some.url/api'
 
 // NOTE from Simpson: don't modify this function at all
 function fakeAjax(url,cb) { // 'cb' stands for 'callback', i.e., a callback function
-  setTimeout(function fakeLoadingDelay() {
+  setTimeout(function fakeLoadingDelay() { // this function expression is only named ('fakeLoadingDelay') for clarity. Simpson likes to do this kind of thing
     cb([
-      "A Song of Ice and Fire",
-      "The Great Gatsby",
-      "Crime & Punishment",
-      "Great Expectations",
-      "You Don't Know JS"
+      'A Song of Ice and Fire',
+      'The Great Gatsby',
+      'Crime & Punishment',
+      'Great Expectations',
+      'You Don\'t Know JS'
     ])
   },500)
 }
 //////////////////////////////////////////////////////
 
-function loadBooks(theBookshelf) { // Note that 'theBookshelf' is a parameter representing an instance of the Bookshelf class.
-  fakeAjax(BOOK_API, function onBooks(bookNamesArr) { // this function expression is only named ('onBooks') for clarity. Simpson likes to do this kind of thing
+function loadBooks(theBookshelf) { // Note that 'theBookshelf' is a parameter representing a new instance of the 'Bookshelf' class.
+  fakeAjax(BOOK_API, function onBooks(bookNamesArr) { // this function expression is only named ('onBooks') for clarity...
     for (let bookName of bookNamesArr) {
       theBookshelf.addFavoriteBook(bookName)
     }
@@ -42,12 +45,3 @@ function loadBooks(theBookshelf) { // Note that 'theBookshelf' is a parameter re
 
 let myBooks = new Bookshelf()
 loadBooks(myBooks)
-
-/* What's logged (here just so I don't need to share on Vercel as well):
-
-Line 11: Favorite Books: 3
-Line 13: A Song of Ice and Fire
-Line 13: Crime & Punishment
-Line 13: You Don't Know JS
-
-*/
